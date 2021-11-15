8b449f02b9ecc06925a77561afcbfa1050ee83ed
subigyan <asubigyan@gmail.com>
2021-11-15 23:08:52 +0545

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import Card from "./components/Card";
 import AddBtn from "./components/AddBtn";
 import Modal from "./components/Modal";
